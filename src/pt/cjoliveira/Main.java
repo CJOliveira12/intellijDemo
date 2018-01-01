bf45b18581469ca89a8d10de7273fb16b0acdd24
mr_mufy <mr.mufy@gmail.com>
2018-01-01 22:34:22 +0000

    new messagedddd kkkkkk

--- src/pt/cjoliveira/Main.java
+++ src/pt/cjoliveira/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("new file");
         System.out.println("new file");
     }
 }
